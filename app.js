e39269e8273a03bb9ca6dc65a426bc9f5d026bba
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-09-21 18:14:50 +0300

    правки

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb', {
