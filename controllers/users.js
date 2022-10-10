f0c10e46b8269509a821a135effdc3e2373ea7f1
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-11 01:28:00 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -76,11 +76,14 @@ module.exports.createUser = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-
-  return User.findUserByCredentials(email, password)
+  if (!email || !password) {
+    next(new BadRequestError('Требуется ввести почту и пароль'));
+  }
+  User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.cookie('authorization', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).send({ message: 'Авторизация прошла успешно!' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true });
+      return res.status(STATUS_OK).send({ token });
     })
     .catch(next);
 };
