2b9e0148b7c2ff8a58d44a6fe2a4496c4a20ec20
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-10 16:40:33 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import { celebrate, Joi } from 'celebrate';
-import auth from '../middelewares/auth';
+
 import { validate } from '../utils/validate';
 // import { validateUrl } from '../utils/validateUrl';
 
@@ -7,7 +7,12 @@ import {
   getAllUsers, getUser, updateUser, updateAvatar, login, createUser, getUserInfo,
 } from '../controllers/users';
 
+const bodyParser = require('body-parser');
+
 const routerUser = require('express').Router();
+
+routerUser.use(bodyParser.json());
+routerUser.use(bodyParser.urlencoded({ extended: true }));
 
 routerUser.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -27,22 +32,22 @@ routerUser.post('/signup', celebrate({
   }),
 }), createUser);
 
-routerUser.get('/users', auth, getAllUsers);
+routerUser.get('/users', getAllUsers);
 
-routerUser.get('/users/me', auth, getUserInfo);
+routerUser.get('/users/me', getUserInfo);
 
 routerUser.get('/users/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().custom(validate, 'ObjectId validation'),
   }),
-}), auth, getUser);
+}), getUser);
 
 routerUser.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
-}), auth, updateUser);
+}), updateUser);
 
 routerUser.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
@@ -51,6 +56,6 @@ routerUser.patch('/users/me/avatar', celebrate({
       .regex(/^https?:\/\/(www.){0,1}([0-9a-zA-Z_-]+\.){1,3}[a-zA-Z]+[A-Za-z0-9-._~:/?#[\]@!$&'()*+,;=]+#?$/m)
       .required(),
   }),
-}), auth, updateAvatar);
+}), updateAvatar);
 
 export default routerUser;
