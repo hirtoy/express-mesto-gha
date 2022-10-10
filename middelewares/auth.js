5d9ffa52a32ffced639bcbe74ccaaeb002fd912a
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-10 13:11:19 +0300

    ghfdrb

--- middelewares/auth.js
+++ middelewares/auth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.cookies;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (error) {
-    throw new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
--- utils/RegExp.js
+++
@@ -1,3 +0,0 @@
-const RegExp = /https?:\/\/(www\.)?\d?\D{1,}#?/;
-
-module.exports = RegExp;
