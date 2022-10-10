9133e2349eea80e422664fd7c413efb3c6e1e631
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-11 02:15:38 +0300

    Update auth.js

--- middelewares/auth.js
+++ middelewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../error/unauthorized-errors');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+// const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
@@ -17,7 +17,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, 'dev-secret');
   } catch (error) {
     // eslint-disable-next-line new-cap
     next(new UnauthorizedError('Необходима авторизация'));
