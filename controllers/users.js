924eaa58abab8a780c62f1b1a254fe2c76841b86
hirtoy <97770904+hirtoy@users.noreply.github.com>
2022-10-11 01:16:27 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -80,7 +80,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).send({ token });
+      res.status(STATUS_OK).cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).send({ message: 'Авторизация прошла успешно!' });
     })
     .catch(next);
 };
